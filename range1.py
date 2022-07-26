2c709ce316ef92e050685bea53f6ef875495d1b9
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-26 15:36:15 +0530

    Add files via upload

---
+++ range1.py
@@ -0,0 +1,52 @@
+# range is going to work forloop without forloop we cannot use range 
+# a=range(5)
+# print(a)   # range(0,5)
+# range(0,5) but iam not getting where to where if range is work compalsary require forloop
+# for pyt in range(5):
+    # print(pyt)        #0,1,2,3,4
+# for goutham in range(20):
+    # print(goutham)            #0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19
+# if we can use for before space that time autometically IndentationError answer will come 
+# for abc in range(5):
+    # print(abc)         #0,1,2,3,4
+
+# for i in range(1,11):
+    # print(i)            # vertical answer
+#  print(i,end=', ')       #    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+# for g in range(1,2000):
+# for g in range(1,500000,1000):    # this is increase
+# for g in range(500000,1,-1000): 
+# for g in range(-1,-500000,-1000):    # this is we can use -numbers
+    # print(g,end=' ')
+# for i in range(20,11):
+#  print(i)               # this type range cannot possible
+# for j in range(1,6):
+    # print(j)             # 1,2,3,4,5 answer verticle
+# for g in range(1,21,2):
+    # print(g)
+# for a in range(0,20,2):
+    # print(a)
+# for b in range(21,0,-1):
+    # print(b)                 # riverse answer will come
+# for b in range(20,0,-2):
+    # print(b)
+# for c in range(19,0,-2):
+    # print(c)
+# n=int(input("enter the value of n"))
+# for i in range(1,n+1):
+# for i in range(1,n+2):
+# for i in range(1,n-1):
+    # print(i,end="\n")   #backslash n means next line come
+# print(n)
+# for j in range(2,30,2):
+    # print(j,end=' ')
+# for j in range(10,0,-1):   # answer will come 1 to 10
+        # print(j)
+# for j in range(10,-1,-1):
+    # print(j)              # answer will come 0 to 10
+# n=[1,4.5,'python']
+# for i in n[::-1]:
+    # print(i)             # rivers answer will come
+# v='programming'
+# for e in v[::2]:
+    # print(e)        #[::2] means skipping two times
